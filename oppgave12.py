80605714c9c759c0efc5c98b27cb7b545d400370
Kristian Skibb Skibrek <kristianskibrek@protonmail.com>
2023-10-20 08:20:56 +0200

    use pi units

--- oppgave12.py
+++ oppgave12.py
@@ -10,15 +10,18 @@ h = 1
 vinkel_liste = np.linspace(start, end, 361)
 
 def s(phi) -> float:
-    if phi < math.pi / 2:
-        phi = phi/(math.pi/2)
+
+    #konverter så gradene faller innenfor intervallet 0, 4
+    phi = phi/(math.pi/2)
+
+    if phi < 1:
         return p(phi)
-    if phi <= math.pi:
+    if phi <= 2:
         return 2 * h
-    if phi < math.pi + math.pi / 2:
-        phi = phi/(math.pi/2)
+    if phi < 3:
         return q(phi)
-    if phi <= 2 * math.pi:
+    #if phi <= 4:
+    else:
         return h
 
 def p(phi) -> float:
@@ -27,14 +30,15 @@ def p(phi) -> float:
 def q(phi) -> float:
     return p(3 - phi)
 
-kam_x = []
 kam_y = []
+
 for vinkel in vinkel_liste:
     kam_y.append(s(vinkel))
 
 print(kam_y)
 
 plt.plot(vinkel_liste, kam_y, "o")
+plt.xticks([0, math.pi/2, math.pi, 3* math.pi / 2, 2 * math.pi], ['0', 'π/2', 'π', '3π/2', '2π'])
 plt.show()
 
 
