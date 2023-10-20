c76c8e1bc5de0e5dd39cbf2d086fb18d31e7569d
Kristian Skibb Skibrek <kristianskibrek@protonmail.com>
2023-10-20 08:27:37 +0200

    graph formatting og docstrings/kommentarer"

--- oppgave12.py
+++ oppgave12.py
@@ -7,38 +7,45 @@ end = 2*math.pi
 number_of_points = 361
 h = 1
 
+# Lager 361 punkter mellom start og slutt
 vinkel_liste = np.linspace(start, end, 361)
 
-def s(phi) -> float:
+def s(phi: float) -> float:
+    """Tar imot en vinkel mellom 0 og 2π og regner ut """
 
     #konverter så gradene faller innenfor intervallet 0, 4
     phi = phi/(math.pi/2)
 
+    #s som definert i oppgaveteksten
     if phi < 1:
         return p(phi)
     if phi <= 2:
         return 2 * h
     if phi < 3:
         return q(phi)
-    #if phi <= 4:
     else:
         return h
 
 def p(phi) -> float:
+    """p funksjon fra oppgave 6"""
     return (6*phi**5 - 15*phi**4 + 10*phi**3 + h)
 
 def q(phi) -> float:
+    """q funksjon fra oppgave 8"""
     return p(3 - phi)
 
 kam_y = []
 
+#Evaluer funksjonen s for punktene og lagre dem i en liste
 for vinkel in vinkel_liste:
     kam_y.append(s(vinkel))
 
 print(kam_y)
 
+#plot funksjonen
 plt.plot(vinkel_liste, kam_y, "o")
 plt.xticks([0, math.pi/2, math.pi, 3* math.pi / 2, 2 * math.pi], ['0', 'π/2', 'π', '3π/2', '2π'])
+plt.yticks([1, 2], ["1h", "2h"])
 plt.show()
 
 
