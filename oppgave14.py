e99ad0956866b7bd1f1d0c79555cc04270605167
Kristian Skibb Skibrek <kristianskibrek@protonmail.com>
2023-10-24 12:02:49 +0200

    remove debug info

--- oppgave14.py
+++ oppgave14.py
@@ -32,11 +32,6 @@ kam_y = []
 for vinkel in vinkel_liste:
     kam_y.append(math.sin(vinkel)*s(vinkel))
     kam_x.append(math.cos(vinkel)*s(vinkel))
-
-print(len(kam_y))
-print(len(kam_x))
-
-#plt.plot(vinkel_liste, kam_y, "o")
 plt.plot(kam_x, kam_y, "o")
 plt.show()
 
